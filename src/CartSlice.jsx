5ce828093f1c70c141769904ba3b8228d26f3458
charmaineneo <56992719+charmaineneo@users.noreply.github.com>
2024-10-23 16:15:22 +0800

    State management using Redux

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -10,12 +10,25 @@ export const CartSlice = createSlice({
     
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
+  addItem: (state, action) => {
+    const { name, image, cost } = action.payload;
+    const existingItem = state.items.find(item => item.name === name);
+    if (existingItem) {
+      existingItem.quantity++;
+    } else {
+      state.items.push({ name, image, cost, quantity: 1 });
+    }
+  }
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
